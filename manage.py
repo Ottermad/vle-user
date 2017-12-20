67b309e5b0645dcb79c1e9297c15d0be1bc8bbb6
Charles Thomas <charlie.thomas@attwoodthomas.net>
2017-12-20 01:27:29 +0000

    Prepare for k8s

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import logging
 
 from flask_script import Manager
 
-from flask_migrate import Migrate, MigrateCommand, upgrade
+from flask_migrate import upgrade, migrate, init, Migrate
 
 from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError
@@ -11,12 +11,9 @@ from sqlalchemy.exc import ProgrammingError
 from app import create_app, db
 from config import DATABASE_NAME
 
-app = create_app()
 
-migrate = Migrate(app, db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+manager = Manager(create_app)
+manager.add_option("-c", "--config", dest="config_name", required=False)
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 
@@ -25,7 +22,11 @@ logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 def create_database():
     """Create database."""
     logging.info("Creating database")
-    engine = create_engine("postgresql://postgres:postgres@db/postgres")
+    engine = create_engine("postgresql://{}:{}@{}/postgres".format(
+        manager.app.config['PG_USER'],
+        manager.app.config['PG_PASSWORD'],
+        manager.app.config['PG_HOST']
+    ))
     conn = engine.connect()
     conn.execute("commit")
 
@@ -39,9 +40,34 @@ def create_database():
 
 
 @manager.command
+def test():
+    print(dir(manager.app.config))
+    print(manager.app.config)
+
+@manager.command
+def db_migrate():
+    m = Migrate(manager.app, db)
+    migrate()
+
+
+@manager.command
+def db_upgrade():
+    m = Migrate(manager.app, db)
+    upgrade()
+
+
+@manager.command
+def db_init():
+    m = Migrate(manager.app, db)
+    init()
+
+
+@manager.command
 def run():
     """Run the app."""
-    app.run(host="0.0.0.0", port=80)
+    create_database()
+    db_upgrade()
+    manager.app.run(host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     manager.run()
