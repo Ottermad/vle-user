c89f2cf6fbf7977f76c1eb07ee6a39d3d75b12b3
Charles Thomas <charlie.thomas@attwoodthomas.net>
2017-12-23 23:59:17 +0000

    Update permissions on user listing

--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,10 @@
 from internal.decorators import permissions_required
+from internal.exceptions import UnauthorizedError
 from app.user.form_functions import create_form, list_forms, edit_form, \
     delete_form, form_detail
 from app.user.user_functions import user_listing, user_create, \
     current_user_details, user_update, user_detail, user_delete, authenticate
-from flask import Blueprint, request
+from flask import Blueprint, request, g
 from app import services
 
 user_blueprint = Blueprint("user", __name__, url_prefix="/user")
@@ -21,14 +22,15 @@ def test():
 
 
 @user_blueprint.route("/user", methods=["GET", "POST"])
-@permissions_required({'Administrator'})
 def user_listing_or_create_view():
     """Route to create User from a POST request."""
     if request.method == "GET":
         return user_listing(request)
 
     if request.method == "POST":
-        return user_create(request)
+        if g.user.has_permissions({'Administrator'}):
+            return user_create(request)
+        raise UnauthorizedError()
 
 
 @user_blueprint.route("/user/<int:user_id>", methods=["PUT", "GET", "DELETE"])
