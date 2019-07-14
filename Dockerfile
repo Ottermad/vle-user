61a66b513dcf4c330d98375e47017d0e045ec338
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2019-07-15 00:04:52 +0100

    Finish Minikube setup

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ RUN mkdir /code
 WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
-RUN pip install git+https://ottermad:af7e0fa5f87f3823703d9e58c7626f0fce6c8c41@github.com/Ottermad/vle-internals.git
-RUN pip install git+https://ottermad:af7e0fa5f87f3823703d9e58c7626f0fce6c8c41@github.com/Ottermad/services_extension.git
+RUN pip install git+https://github.com/Ottermad/vle-internals.git
+RUN pip install git+https://github.com/Ottermad/services_extension.git
 
 ADD . /code/
 
--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', "this_needs_to_be_more_secure")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVICE_NAME = 'user'
+    K8S_SERVICE_NAME = 'VLE_USER'
 
 
 class Development(Config):
@@ -42,9 +43,9 @@ class Migrations(Development):
 
 class Minikube(Config):
     DEBUG = True
-    PG_PASSWORD = os.environ.get('PG_PASSWORD', 'postgres')
-    PG_HOST = 'killer-gibbon-postgresql'
-    PG_USER = 'postgres'
+    PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'postgres')
+    PG_HOST = os.environ.get('POSTGRES_HOST', 'killer-gibbon-postgresql')
+    PG_USER = os.environ.get('POSTGRES_USER', 'postgres')
     SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
         PG_USER, PG_PASSWORD, PG_HOST, DATABASE_NAME)
 
